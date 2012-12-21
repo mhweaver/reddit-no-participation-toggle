7a31257f8faab0377d9298c442490b571a80b4f4
mhweaver <mhweaver@gmail.com>
2012-12-21 15:02:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Reddit No Participation Toggle
+
+[Reddit No Participation Toggle](https://chrome.google.com/webstore/detail/mjpnnnikhngjjoapjdcdfepkenphaobi) is a Google Chrome extension that allows you to disable the new "No Participation" feature many subreddits have recently implemented.
+
+This extension adds a "[NP on/off]" link to all reddit pages (in the upper-
+right, next to the "preferences" link to allow you to quickly toggle NP). When NP
+is disabled, this extension replaces every np.reddit.com and www.np.reddit.com link
+with its corresponding non-NP link. Note that this does not disable NP functionality, but
+instead attempts to make it so they are never encountered to begin with. As such,
+if you go directly to a NP link, you will still not be able to vote or comment (although
+this extension has the added benefit of turning all of the links on an NP page back into non-NP links).
+
+Reddit No Participation Toggle generally plays well with the Reddit Enhancement Suite 
+(RES) everywhere RES normally works (as far as I know).
---
+++ src/np.js
@@ -0,0 +1,67 @@
+if (localStorage['enableNP'] == undefined) {
+  localStorage['enableNP'] = true
+}
+
+document.onreadystatechange = function(e) {
+  if (document.readyState != "complete") return
+  var enableNPText = (localStorage['enableNP'] == "true") ? 'on' : 'off'
+  enableDisable()
+ 
+  var toggleLink = document.createElement("a")
+  toggleLink.href = "javascript:void(0)"
+  toggleLink.id = "toggleNP"
+  toggleLink.innerText = "[NP " + enableNPText + "]"
+  
+  var separator = document.createElement("span")
+  separator.className = "separator"
+  separator.innerText = "|"
+  
+  var preferences = document.querySelector('a.pref-lang').parentNode.parentNode
+  preferences.parentNode.insertBefore(toggleLink, preferences.nextSibling)
+  preferences.parentNode.insertBefore(separator, toggleLink)
+  
+    
+    
+  document.querySelector('#toggleNP').addEventListener('click', function(e) {
+    toggleNP()
+    this.innerText = '[NP ' + (localStorage['enableNP'] == "true" ? 'on' : 'off') + ']'
+  }, false)
+}
+
+// Warning: Since this works with NodeLists, changes will be made to the actual
+// NodeList object (as opposed to Array.map() which returns a new Array
+// object), so this is NOT a pure function, unlike most map() functions
+NodeList.prototype.map = function(fn) {
+  for (i in this) {
+    fn(this[i])
+  }
+  return this
+}
+
+
+function toggleNP() {
+  localStorage['enableNP'] = localStorage['enableNP'] == "true" ? "false" : "true"
+  if (localStorage['enableNP'] == "true") // if NP was just enabled
+    alert("NP will be re-enabled after the page is reloaded.")
+  enableDisable()
+}
+function disable(element) {
+  pattern = /http:\/\/[^/]*np\.reddit\.com\//i;
+  if (element.href != undefined && element.href.match(pattern)) {
+    element.href = element.href.replace(pattern, "http://www.reddit.com/")
+  }
+}
+function enable(element) {
+  //pattern = /http:\/\/[^/]*\reddit\.com\//i;
+  //if (element.dataset != undefined && element.dataset.isnp)
+  //  element.href = element.href.replace(pattern, "http://np.reddit.com/")
+}
+function enableDisable() {
+  if (localStorage['enableNP'] == 'true') {
+    //document.querySelectorAll("a").map(enable)
+  } else {
+    document.querySelectorAll("a[href^='http://np.']").map(disable)
+    document.querySelectorAll("a[href^='http://www.np.']").map(disable)
+  }
+}
+window.addEventListener('hashchange', enableDisable)
